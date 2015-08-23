c39ab47127e9a7a228d65fbfdf18f7c05e8faabd
Oskar Gewalli <gewalli@gmail.com>
2015-08-23 12:43:27 +0200

    Created the library in vs 2015 instead

--- SomeLibrary/AssemblyInfo.fs
+++ SomeLibrary/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace Perch.AssemblyInfo
+﻿namespace SomeLibrary.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("SomeFSharpLibrary")>]
+[<assembly: AssemblyTitle("SomeLibrary")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("SomeFSharpLibrary")>]
+[<assembly: AssemblyProduct("SomeLibrary")>]
 [<assembly: AssemblyCopyright("Copyright ©  2015")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
@@ -22,7 +22,7 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("05e3cbc0-b2b7-4c80-a627-bc876f48ee2e")>]
+[<assembly: Guid("40545dd5-bb1b-4a37-bded-99550698b03d")>]
 
 // Version information for an assembly consists of the following four values:
 // 
